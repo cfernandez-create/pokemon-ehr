edbbfcb0ef1769bf2eaf87d6245344582bda3b28
cfernandez-create <131753147+cfernandez-create@users.noreply.github.com>
2023-05-14 01:55:01 -0700

    commmmit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-=======
 import Header from './components/Header';
 import SideNav from './components/SideNav';
 import TopNav from './components/TopNav';
 import SideImage from './components/SideImage';
 import Main from './components/Main';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Header />
       <SideImage />
       <SideNav />
       <TopNav />
       <Main />
     
->>>>>>> Stashed changes
     </div>
   );
 }
