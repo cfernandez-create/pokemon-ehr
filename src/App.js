ca13a15f4031161a6f70734afaa9e530676f148e
unknown <131753147+cfernandez-create@users.noreply.github.com>
2023-05-30 20:18:03 -0700

    Implemented script that fixes viewport height on mobile devices

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 // App.js
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import Header from "./components/Header";
 import SideNav from "./components/SideNav";
 import Main from "./components/Main";
-import {fetchData} from "./functions/fetchData"
-import {baseURL} from "./utils/constant"
+
 
 
 function App() {
-  const [admitData, setAdmitData] = useState([]);
- 
- 
 
-  useEffect (() => {
-    fetchData(baseURL, setAdmitData);
-  }, []);
-
-  
   return (
     <div className="App">
-      <Header/>
       <SideNav/>
       <Main/>
     </div>
